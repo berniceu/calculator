913cf4649e54877d1830125d35e69c0db048c2ce
berniceu <berniceuwituze@gmail.com>
2023-07-21 22:49:20 +0200

    store all html values in js and assign to variables

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equals");
+let decimal = document.querySelector(".decimal");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operators");
+
+let previousScreen = document.querySelector(".previous");
+let currentScreen = document.querySelector(".current");
