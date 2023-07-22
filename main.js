50bad7a7ae6b6a243bf069542ecac99c6d99581f
berniceu <berniceuwituze@gmail.com>
2023-07-22 20:37:38 +0200

    add functions in main.js file

--- main.js
+++ main.js
@@ -7,3 +7,55 @@ let operators = document.querySelectorAll(".operators");
 
 let previousScreen = document.querySelector(".previous");
 let currentScreen = document.querySelector(".current");
+
+numbers.forEach((number) => number.addEventListener("click", function(e){
+    handleNumber(e.target.textContent)
+    currentScreen.textContent = currentValue;
+}))
+
+operators.forEach((op) => op.addEventListener("click", function(e){
+    operate(e.target.textContent)
+    previousScreen.textContent = `${previousValue} ${op}`;
+    currentScreen.textContent = currentValue;
+}))
+
+clear.addEventListener("click", function(){
+    currentValue = '';
+    previousValue = '';
+    op  = '';
+    previousScreen.textContent = currentValue;
+    currentScreen.textContent = currentValue;
+})
+equal.addEventListener("click", function(){
+    calculate()
+})
+
+function handleNumber(num){
+    if(currentValue.length <= 9){
+        currentValue += num;
+    }
+}
+
+function operate(op){
+    previousValue = currentValue;
+    currentValue = '';
+
+}
+
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(op === "+"){
+        previousValue += currentValue;
+    } else if(op === "-"){
+        previousValue -= currentValue;
+    } else if(op === "/"){
+        previousValue /= currentValue;
+    } else if(op === "x"){
+        previousValue *= currentValue;
+    }
+
+    previousValue = previousValue.toString();
+    currentValue = currentValue.toString()
+}
